34656d3c9ad8f0a855ccf366d262d01ec1764963
nasa181 <tanawat181@hotmail.com>
2016-03-29 22:37:02 +0700

    google map4

--- BKK_Travel/resources/views/attraction.blade.php
+++ BKK_Travel/resources/views/attraction.blade.php
@@ -1,7 +1,7 @@
 @extends('master')
 @section('center_page')
     <div class="row">
-        <div class="col-sm-12"><img src="cooltext165456723410492.png" class="img-rounded center-block" id="img1" width=75%></div>
+        <div class="col-sm-12"><img src="/cooltext165456723410492.png" class="img-rounded center-block" id="img1" width=75%></div>
     </div>
     <div id="map"></div>
     <script>
--- BKK_Travel/resources/views/info_attr.blade.php
+++ BKK_Travel/resources/views/info_attr.blade.php
@@ -9,6 +9,18 @@
         <div class="col-sm-12"><span>{{$attr->entrance_fee}}</span></div>
         <div class="col-sm-12"><span>{{$attr->oc_time}}</span></div>
     </div>
+    <div id="map"></div>
+    <script>
+        var map;
+        function initMap() {
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: {lat: {{$attr->lat}}, lng: {{$attr->long}}},
+                zoom: 8
+            });
+        }
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
+            async defer></script>
 
 
 @stop

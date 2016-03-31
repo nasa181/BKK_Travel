aa0e54602a4e243ffcccdec2dc56e1fd685d24fb
nasa181 <tanawat181@hotmail.com>
2016-03-31 16:19:43 +0700

    add item2

--- BKK_Travel/resources/views/info_restaurant.blade.php
+++ BKK_Travel/resources/views/info_restaurant.blade.php
@@ -28,6 +28,10 @@
         }
 
         function placeMarkerAndPanTo(latLng, map) {
+            var marker = new google.maps.Marker({
+                position: latLng,
+                map: map
+            });
             map.panTo(latLng);
         }
     </script>

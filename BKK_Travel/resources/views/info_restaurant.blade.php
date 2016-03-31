2128014ba6b31f947a417df804277f8fd9bb9867
nasa181 <tanawat181@hotmail.com>
2016-03-31 17:00:45 +0700

    adding item4`

--- BKK_Travel/resources/views/info_restaurant.blade.php
+++ BKK_Travel/resources/views/info_restaurant.blade.php
@@ -20,13 +20,11 @@
             var mapDiv = document.getElementById('map');
             var map = new google.maps.Map(mapDiv, {
                 zoom: 8,
-                center: new google.maps.LatLng(-34.397, 150.644)
+                center: {lat: 0, lng: 0}
             });
 
-            // We add a DOM event here to show an alert if the DIV containing the
-            // map is clicked.
-            google.maps.event.addDomListener(mapDiv, 'click', function() {
-                window.alert('Map was clicked!');
+            map.addListener('click', function(e) {
+                firebase.push({lat: e.latLng.lat(), lng: e.latLng.lng()});
             });
         }
     </script>

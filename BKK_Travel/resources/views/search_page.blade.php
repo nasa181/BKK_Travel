b08f4c33b830140b9db5aecc008045b33fd8499c
nasa181 <tanawat181@hotmail.com>
2016-04-23 21:20:20 +0700

    let's try

--- BKK_Travel/resources/views/search_page.blade.php
+++ BKK_Travel/resources/views/search_page.blade.php
@@ -1,6 +1,23 @@
 @extends('master')
 @section('center_page')
         <div class="row well">
-            <span>{{$item->title}}</span>
+            @if($attraction != null)
+                @foreach($attraction as $attr)
+                    <a href="/page_travel/info/{{$attr->item_id}}">{{$attr->title}}</a>
+                @endforeach
+            @endif
+            @if($restaurant != null)
+                @foreach($restaurant as $res)
+                    <a href="/page_restaurant/info/{{$res->item_id}}">{{$res->title}}</a>
+                @endforeach
+            @endif
+                <div class="row col-xs-12 " style="color: #000000;">
+                    <h1>Search not found</h1>
+                    <p>How about adding new item for us?</p>
+                </div>
+            <div class="row col-xs-12 ">
+                <a href="/page_attraction/create_new_attraction"><button class="btn btn-success">Add new attraction</button></a>
+                <a href="/page_restaurant/create_new_restaurant"><button class="btn btn-success">Add new restaurant</button></a>
+            </div>
         </div>
 @stop

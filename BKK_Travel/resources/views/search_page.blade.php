0883efabbe79be7d6419d3e768336816124243fa
Earth <lostunevol@gmail.com>
2016-04-25 14:56:42 +0700

    update minor ui

--- BKK_Travel/resources/views/search_page.blade.php
+++ BKK_Travel/resources/views/search_page.blade.php
@@ -14,11 +14,12 @@
             if ($attr->title_picture == "" || $attr->title_picture == null) $background = "green" ;
             else  $background = "url('" . url($attr->title_picture) . "')";
             ?>
+            <a href="/page_travel/info/{{$attr->item_id}}">
             <div class="col-md-6" style="padding: 0px 20px">
                 <div class="item_description " style="padding:10px;overflow:hidden;margin:10px 0px;height: 120px;background: {{$background}} center ;background-size:cover ">
                     <div class="row" style=" text-shadow: 1px 1px black;">
                         <div class="col-xs-12">
-                            <div class="head item_description" style=""><a href="/page_travel/info/{{$attr->item_id}}"><span><h4>{{$attr->title}}</h4></span></a></div>
+                            <div class="head item_description" style=""><span><h4>{{$attr->title}}</h4></span></div>
                         </div>
                         <div class="col-xs-12">
                             <div class="text-info" style="margin: 0px"><span>Price:   {{$attr->entrance_fee}}</span></div>
@@ -29,17 +30,19 @@
                     </div>
                 </div>
             </div>
+            </a>
         @endforeach
         @foreach($restaurant as $res)
         <?php
         if ($res->title_picture == "" || $res->title_picture == null) $background = "green" ;
         else  $background = "url('" . url($res->title_picture) . "')";
         ?>
+            <a href="/page_restaurant/info/{{$res->item_id}}">
             <div class="col-md-6" style="padding: 0px 20px">
                 <div  class=" item_description" style="padding:10px;overflow:hidden;margin:10px 0px;background: green;height: 120px;overflow:hidden;background: {{$background}}  center ;background-size:cover ">
                     <div class="row" style=" text-shadow: 1px 1px black;">
                         <div class="col-xs-12">
-                            <div class="head item_description" style=""><a href="/page_restaurant/info/{{$res->item_id}}"><span><h4>{{$res->title}}</h4></span></a></div>
+                            <div class="head item_description" style=""><span><h4>{{$res->title}}</h4></span></div>
                         </div>
                         <div class="col-xs-12">
                             <div class="text-info" style="margin: 0px"><span>{{$res->food_type}}</span></div>
@@ -53,6 +56,7 @@
                     </div>
                 </div>
             </div>
+            </a>
         @endforeach
     </div>
     @if($restaurant == null && $attraction == null)
@@ -64,7 +68,7 @@
             <hr>
         </div>
     </div>
-    <span>How about sharing your new experience?</span><h3 style="color: blue">Let's add new item</h3>
+    <span>How about sharing your new experience?</span><h3 style="color: white">Let's add new item</h3>
     @if(isset($current_user))
             <div class="row col-xs-12 ">
                 <a href="/page_attraction/create_new_attraction"><button class="btn btn-success">Add new attraction</button></a>
